8eb8525bb74144b8135a030ca607eac40fa7f819
Saqib Ayoub <saqibtelixy@gmail.com>
2024-07-30 16:31:10 +0530

    vid 1.01.40

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -8,7 +8,7 @@ const AddPost = () => {
             <Image src="https://images.pexels.com/photos/21550488/pexels-photo-21550488/free-photo-of-blue-scooter-parked-by-a-wall.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' className=' w-12 h-12 object-cover rounded-full' width={48} height={48} />
             {/* post  */}
             <div className="">
-                {/* Text input */}
+                {/* Text input*/}
                 <div className="flex gap-4n">
                     <textarea name="" id="" placeholder="what's on your mind" className=' bg-slate-100 rounded-lg'></textarea>
                     <Image src="/emoji.png" alt='' className=' w-5 h-5 cursor-pointer self-end' width={20} height={20} />
