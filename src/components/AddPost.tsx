bb0d97b2dc9a015632e7611df5e82997f7bfdb8c
Saqib Ayoub <saqibtelixy@gmail.com>
2024-07-31 23:00:47 +0530

    add like comment and share

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -3,18 +3,36 @@ import React from 'react'
 
 const AddPost = () => {
     return (
-        <div className=" p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+        <div className=" p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
             {/* Avatar */}
             <Image src="https://images.pexels.com/photos/21550488/pexels-photo-21550488/free-photo-of-blue-scooter-parked-by-a-wall.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' className=' w-12 h-12 object-cover rounded-full' width={48} height={48} />
             {/* post  */}
-            <div className="">
+            <div className="  flex-1">
                 {/* Text input*/}
-                <div className="flex gap-4n">
-                    <textarea name="" id="" placeholder="what's on your mind" className=' bg-slate-100 rounded-lg'></textarea>
+                <div className="flex gap-4">
+                    <textarea name="" id="" placeholder="what's on your mind" className='flex-1 bg-slate-100 rounded-lg p-2'></textarea>
                     <Image src="/emoji.png" alt='' className=' w-5 h-5 cursor-pointer self-end' width={20} height={20} />
                 </div>
                 {/* Post options */}
-                <div className=""></div>
+                <div className=" flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+                    <div className=" flex items-center gap-2 cursor-pointer ">
+                        <Image src="/addImage.png" alt='' width={20} height={20} />
+                        Photo
+                    </div>
+                    <div className=" flex items-center gap-2 cursor-pointer">
+                        <Image src="/addVideo.png" alt='' width={20} height={20} />
+                        Video
+                    </div>
+                    <div className=" flex items-center gap-2 cursor-pointer">
+                        <Image src="/poll.png" alt='' width={20} height={20} />
+                        Poll
+                    </div>
+                    <div className=" flex items-center gap-2 cursor-pointer">
+                        <Image src="/addEvent.png" alt='' width={20} height={20} />
+                        Event
+                    </div>
+
+                </div>
             </div>
 
         </div>
---
+++ src/components/Post.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div className=' flex flex-col gap-4 '>
+            {/* User div */}
+            <div className=" flex items-center justify-between">
+                <div className=" flex items-center gap-4 ">
+                    <Image src="https://images.pexels.com/photos/27430540/pexels-photo-27430540/free-photo-of-blue-eyes-with-horse.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" className='w-10 h-10 rounded-full '
+                        height={40} width={40} />
+                    <span className=' font-medium'>M Ayat</span>
+                </div>
+                <Image src="/more.png" alt=""
+                    height={16} width={16} />
+            </div>
+            {/* Description */}
+            <div className=" flex flex-col gap-4">
+                <div className=" w-full min-h-96 relative ">
+                    <Image src="https://images.pexels.com/photos/27402207/pexels-photo-27402207/free-photo-of-jagat-siromani-temple.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""
+                        fill className=' object-cover rounded-md' />
+                </div>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla neque quas nam et vitae, placeat ratione, officiis, expedita fugiat eos quidem? Quo, harum hic beatae quos magni sapiente deserunt ipsum!
+                </p>
+            </div>
+            {/* Interaction*/}
+            <div className=" flex items-center justify-between text-sm my-4">
+                <div className=" flex gap-8">
+                    <div className=" flex items-center gap-4 bg-slate-50 p-2 rounded-xl ">
+                        <Image src="/like.png" alt=""
+                            height={16} width={16} className=' cursor-pointer ' />
+                        <span className=' text-gray-300'>|</span>
+                        <span className=' text-gray-500'>123<span className=' hidden md:inline'> Likes</span></span>
+
+                    </div>
+                    <div className=" flex items-center gap-4 bg-slate-50 p-2 rounded-xl ">
+                        <Image src="/comment.png" alt=""
+                            height={16} width={16} className=' cursor-pointer ' />
+                        <span className=' text-gray-300'>|</span>
+                        <span className=' text-gray-500'>123<span className=' hidden md:inline'> Comments</span></span>
+
+                    </div>
+                </div>
+                <div className="">
+                    <div className=" flex items-center gap-4 bg-slate-50 p-2 rounded-xl ">
+                        <Image src="/share.png" alt=""
+                            height={16} width={16} className=' cursor-pointer ' />
+                        <span className=' text-gray-300'>|</span>
+                        <span className=' text-gray-500'>123<span className=' hidden md:inline'> Shares</span></span>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Post
