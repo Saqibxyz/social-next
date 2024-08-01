ea542a9237fe93e5b60f87574d7b11161d90db32
Saqib Ayoub <saqibtelixy@gmail.com>
2024-08-01 15:39:55 +0530

    add birthdays vid 1.31.30

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import FriendRequest from './FriendRequest'
+import BirthDay from './BirthDay'
+import Ad from './Ad'
 
-const RightMenu = () => {
-  return (
-    <div>RightMenu</div>
-  )
+const RightMenu = ({ userId }: { userId?: string }) => {
+    return (
+        <div className=' flex flex-col gap-6'>
+            <FriendRequest />
+            <BirthDay />
+            <Ad size='md'/>
+
+        </div>
+    )
 }
 
 export default RightMenu
